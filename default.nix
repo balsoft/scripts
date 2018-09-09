37acfe35316d15745b5e25f891737d1844351b19
Alexander Bantyev <balsoft@yandex.ru>
2018-09-09 13:45:36 +0300

    test

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { pkgs, theme, secret, ... }:
 {
-    polybar = import ./polybar { pkgs, secret, theme };
+    polybar = import ./polybar { inherit pkgs; inherit theme; inherit secret; };
     zshrc = builtins.readFile (pkgs.stdenv.mkDerivation rec {
         name = "zshrc-extra.sh";
         src = ./zshrc.sh;
